bb3d76d00fcf95a72a2811c8b7f38bcb733217ac
Om Raja <omraja451@gmail.com>
2025-10-25 23:40:27 +0530

    add gloabal store and createProduct action

---
+++ frontend/src/Store/product.store.js
@@ -0,0 +1,30 @@
+import {create} from "zustand";
+
+export const useProductStore = create((set)=>({
+    products: [],
+
+    setProducts: (products)=>set({
+       products: products,
+    }),
+
+    createProduct: async (newProduct)=>{
+        if(!newProduct.name || !newProduct.price || !newProduct.image)
+            return {success: "false", message: "All fields are required."};
+
+        const response = await fetch("/api/product", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({product: newProduct}),
+        })
+
+        const data = await response.json();
+
+        set((state)=>({
+            products:[...state.products, data.data]
+        }));
+
+        return {success: true, message: "Product created successfully!"};
+    }
+}))
--- frontend/src/pages/CreateProductPage.jsx
+++ frontend/src/pages/CreateProductPage.jsx
@@ -9,18 +9,26 @@ import {
   Field,
 } from "@chakra-ui/react";
 import { useState } from "react";
+import { useProductStore } from "../Store/product.store.js";
 
 const CreateProductPage = () => {
+  const {createProduct} = useProductStore();
+
   const [productDetail, setProductDetail] = useState({
     name: "",
     price: "",
     image: "",
   });
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
 
-    console.log(productDetail);
+    const response = await createProduct(productDetail);
+    const {success, message} = response;
+
+    console.log("Success: ", success);
+    console.log("Message: ", message);
+    
     setProductDetail({
       name: "",
       price: "",
@@ -36,12 +44,7 @@ const CreateProductPage = () => {
         </Heading>
 
         <Box
-          w={{
-            base:"95%",
-            sm: "80%",
-            md: "60%",
-            lg: "50%"
-          }}
+          w={["95%", "80%", "60%", "50%" /*mobile, tablet, desktop */]}
           bgColor={useColorModeValue("gray.300", "gray.700")}
           p={6}
           rounded={"lg"}
@@ -49,12 +52,16 @@ const CreateProductPage = () => {
         >
           <VStack spacing={4}>
             <Field.Root required>
-              <Field.Label>Name<Field.RequiredIndicator/></Field.Label>
+              <Field.Label>
+                Name
+                <Field.RequiredIndicator />
+              </Field.Label>
               <Input
                 placeholder="Poco M2 Pro"
                 name="name"
                 value={productDetail.name}
                 variant="subtle"
+                type="text"
                 onChange={(event) =>
                   setProductDetail({
                     ...productDetail,
@@ -66,40 +73,46 @@ const CreateProductPage = () => {
             </Field.Root>
 
             <Field.Root required>
-              <Field.Label>Price<Field.RequiredIndicator/></Field.Label>
+              <Field.Label>
+                Price
+                <Field.RequiredIndicator />
+              </Field.Label>
               <Input
-              placeholder="15999"
-              name="price"
-              value={productDetail.price}
-              variant="subtle"
-              onChange={(event) =>
-                setProductDetail({
-                  ...productDetail,
-                  price: event.target.value,
-                })
-              }
-            ></Input>
-            <Field.ErrorText>Price is required</Field.ErrorText>
+                placeholder="15999"
+                name="price"
+                value={productDetail.price}
+                variant="subtle"
+                type="number"
+                onChange={(event) =>
+                  setProductDetail({
+                    ...productDetail,
+                    price: event.target.value,
+                  })
+                }
+              ></Input>
+              <Field.ErrorText>Price is required</Field.ErrorText>
             </Field.Root>
             <Field.Root required>
-              <Field.Label>Image Link<Field.RequiredIndicator/></Field.Label>
+              <Field.Label>
+                Image Link
+                <Field.RequiredIndicator />
+              </Field.Label>
               <Input
-              placeholder="pocom2-pro.jpg"
-              name="image"
-              value={productDetail.image}
-              variant="subtle"
-              onChange={(event) =>
-                setProductDetail({
-                  ...productDetail,
-                  image: event.target.value,
-                })
-              }
-            ></Input>
-            <Field.ErrorText>Image link is required</Field.ErrorText>
+                placeholder="pocom2-pro.jpg"
+                name="image"
+                value={productDetail.image}
+                variant="subtle"
+                type="text"
+                onChange={(event) =>
+                  setProductDetail({
+                    ...productDetail,
+                    image: event.target.value,
+                  })
+                }
+              ></Input>
+              <Field.ErrorText>Image link is required</Field.ErrorText>
             </Field.Root>
 
-            
-            
             <Button mt={3} w={"full"} onClick={handleSubmit}>
               Add Product
             </Button>
