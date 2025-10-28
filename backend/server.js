a5d73fb69eba51e02ca1ee28ca4aa6e740b31447
Om Raja <omraja451@gmail.com>
2025-10-28 11:51:26 +0530

    configure for deployment as monolithic architecture

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,34 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import path from "path";
 import express from "express";
 import connectToDB from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 
-
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 //routes
 app.use("/api/product", productRoutes);
 
-app.get("/", (req, res)=>{
-    res.json({success: true, message: "You are live"});
-})
+const __dirname = path.resolve(); // __dirname exists only in type = commonjs
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("You are in development mode");
+  });
+}
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=>{
-    connectToDB();
-    console.log("App is listening at port", PORT);
-})
+app.listen(PORT, () => {
+  connectToDB();
+  console.log("App is listening at port", PORT);
+});
